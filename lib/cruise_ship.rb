448f16dbafb2d6dfe0239ecddb885b1d9d3e019e
Min Hee  Park <minheekangg@gmail.com>
2018-10-02 17:17:30 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -12,7 +12,7 @@ def select_winner(passengers)
   # add the code snippet here!
   winner = ""
   passengers.each do |suite, name|
-    if name.start_with?("A") && suite == :suite_a
+    if suite == :suite_a && name.start_with?("A") 
       winner << name
     end
   end
